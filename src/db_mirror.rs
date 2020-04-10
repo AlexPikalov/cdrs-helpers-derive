fdef307fb3ffb28a83242b23296e9e25a053e2c8
Jasper Visser <jasper.visser@keylane.com>
2020-04-10 12:02:04 +0200

    Names are now being showed

--- src/db_mirror.rs
+++ src/db_mirror.rs
@@ -1,18 +1,20 @@
-use common::{get_ident_string, struct_fields};
+use common::struct_fields;
 use quote;
 use syn;
+use syn::Ident;
 
 pub fn impl_db_mirror(ast: &syn::DeriveInput) -> quote::Tokens {
     let name = &ast.ident;
     let fields = struct_fields(ast);
+    let fields: Vec<Ident> = fields.iter().map(|f| f.ident.clone().unwrap()).collect();
 
     quote! {
         impl #name {
             pub fn insert_query() -> &'static str {
                 concat!("insert into ", stringify!(#name), "(",
-                  #(stringify!(#fields))*
-                 , ") values (",
-                 // Add the amount of '?' for the amount of fields
+                  #(stringify!(#fields, ),)*
+                 ") values (",
+                 #("?"),*
                  ")")
             }
         }
